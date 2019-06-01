a8d9feb4c33e5236224e79ef6e2808603e032506
Jose Carlos <51250539+jcarlos5@users.noreply.github.com>
2019-06-01 18:57:26 -0500

    estructura básica de la web

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
+    var altura = $("#navegacion").offset().top;
 
+  	$(window).on('scroll', function(){
+  		if ( $(window).scrollTop() > altura ){
+  			$("#navegacion").addClass('nav-fixed');
+  		} else {
+  			$("#navegacion").removeClass('nav-fixed');
+  		}
+  	});
+});
