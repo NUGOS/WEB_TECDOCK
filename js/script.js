098e5b36dc1500836d28d0530081a98307486ea7
Jose Carlos <51250539+jcarlos5@users.noreply.github.com>
2019-06-01 22:40:32 -0500

    Primer Presentable

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    var altura = $("#navegacion").offset().top;
 
+    //SCRIPT PARA FIJAR LA BARRA AL HACER SCROLL
+    var altura = $("#navegacion").offset().top;
   	$(window).on('scroll', function(){
   		if ( $(window).scrollTop() > altura ){
   			$("#navegacion").addClass('nav-fixed');
@@ -8,4 +9,39 @@ $(document).ready(function () {
   			$("#navegacion").removeClass('nav-fixed');
   		}
   	});
+
+    //SCRIPT PARA LA BARRA DE NAVEGACIÓN
+    var $els = $('.menu a, .menu header');
+    var count = $els.length;
+    var grouplength = Math.ceil(count/3);
+    var groupNumber = 0;
+    var i = 1;
+    $('.menu').css('--count',count+'');
+    $els.each(function(j){
+        if ( i > grouplength ) {
+            groupNumber++;
+            i=1;
+        }
+        $(this).attr('data-group',groupNumber);
+        i++;
+    });
+
+    $('.menu footer button').on('click',function(e){
+        e.preventDefault();
+        $els.each(function(j){
+            $(this).css('--top',$(this)[0].getBoundingClientRect().top + ($(this).attr('data-group') * -15) - 20);
+            $(this).css('--delay-in',j*.1+'s');
+            $(this).css('--delay-out',(count-j)*.1+'s');
+        });
+        $('.menu').toggleClass('closed');
+        e.stopPropagation();
+    });
+
+    // run animation once at beginning for demo
+    setTimeout(function(){
+        $('.menu footer button').click();
+        setTimeout(function(){
+            $('.menu footer button').click();
+        }, (count * 100) + 500 );
+    }, 1000);
 });
