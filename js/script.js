7f79da1b8dde2565960fe8d986b554f780e79c36
Jose Carlos <51250539+jcarlos5@users.noreply.github.com>
2019-06-02 23:35:21 -0500

    Segundo Avance

--- js/script.js
+++ js/script.js
@@ -10,38 +10,33 @@ $(document).ready(function () {
   		}
   	});
 
-    //SCRIPT PARA LA BARRA DE NAVEGACIÓN
-    var $els = $('.menu a, .menu header');
-    var count = $els.length;
-    var grouplength = Math.ceil(count/3);
-    var groupNumber = 0;
-    var i = 1;
-    $('.menu').css('--count',count+'');
-    $els.each(function(j){
-        if ( i > grouplength ) {
-            groupNumber++;
-            i=1;
-        }
-        $(this).attr('data-group',groupNumber);
-        i++;
-    });
-
-    $('.menu footer button').on('click',function(e){
-        e.preventDefault();
-        $els.each(function(j){
-            $(this).css('--top',$(this)[0].getBoundingClientRect().top + ($(this).attr('data-group') * -15) - 20);
-            $(this).css('--delay-in',j*.1+'s');
-            $(this).css('--delay-out',(count-j)*.1+'s');
-        });
-        $('.menu').toggleClass('closed');
-        e.stopPropagation();
-    });
-
-    // run animation once at beginning for demo
-    setTimeout(function(){
-        $('.menu footer button').click();
-        setTimeout(function(){
-            $('.menu footer button').click();
-        }, (count * 100) + 500 );
-    }, 1000);
+    //SCRIPTS PARA ANIMACIONES DE ANUNCIOS
+    var anun1 = $("#anuncio1").offset().top;
+  	$(window).on('scroll', function(){
+  		if ( $(window).scrollBottom() > anun1 ){
+  			$("#anuncio1").addClass('e-izquierda');
+  		} else {
+  			$("#anuncio1").removeClass('e-izquierda');
+  		}
+  	});
+    var anun2 = $("#anuncio2").offset().top;
+  	$(window).on('scroll', function(){
+  		if ( $(window).scrollBottom() > anun1 ){
+  			$("#anuncio2").addClass('e-derecha');
+  		} else{
+  			$("#anuncio2").removeClass('e-derecha');
+  		}
+  	});
+    var anun3 = $("#anuncio3").offset().top;
+  	$(window).on('scroll', function(){
+  		if ( $(window).scrollBottom() > anun1 ){
+  			$("#anuncio3").addClass('e-arriba');
+  		} else {
+  			$("#anuncio3").removeClass('e-arriba');
+  		}
+  	});
 });
+
+$.fn.scrollBottom = function() {
+    return this.scrollTop() + this.height();
+};
